cb520cce1df8fc099e1d564d5bb0b624425c3430
Ulisesuarez <ulises203@msn.com>
2017-11-23 17:46:51 +0100

    a medias

--- bubble.py
+++ bubble.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
-from random import shuffle
+import random
 
 
 def createRandomList(length):
-    lista=list(range(lenght))
-    lista
+    assert isinstance(length,int)
+    listaRandom=list(range(length))
+    random.shuffle(listaRandom)
+    assert len(listaRandom)==length
+    
+    return listaRandom
     # recibe como parametro la longitud de la lista
     # crea una lista de numero enteros
     # la "mezcla" = desordena
@@ -18,6 +22,9 @@ def display(lista):
 
 
 def less(a, b):
+    #assert isinstance(a,int) and isinstance(b,int) or isinstance(a,str) and isinstance(b,str)
+    return a<b
+     
     # comprueba si a es menor que b
     # devuelve un boolean
     # recibe dos elementos
@@ -25,6 +32,9 @@ def less(a, b):
 
 
 def exchange(lista, i, j):
+    
+    lista[i], lista[j]=lista[j], lista[i]
+    assert isExchanged(lista, i, j)
     # intercambia dos elementos de posicion en la lista
     # recibe la lista, la posicion i y la posicion j
     # devuelve None
@@ -32,17 +42,33 @@ def exchange(lista, i, j):
 
 
 def isExchanged(lista, i, j):
+    
+    return lista[i]<lista[j]
     # comprueba si el elemento en la posicion i
     # es menor que el elemento en la posicion j
     # devuelve un boolean
 
 
 def isSorted(lista):
+    for(offset,element) in enumerate(lista[:-1]):
+        if element >lista[offset+1]:
+            return False
+    return True
     # comprueba si la lista esta oredenada
     # devuelve un boolean
 
 
 def bubbleSort(lista):
+    while not isSorted(lista):
+        for indice in range(len(lista)-1):
+            #if indice<len(lista)-1:
+                if less(lista[indice],lista[indice+1]):
+                    exchange(lista,lista[indice],lista[indice+1])
+                    display(lista)
+        
+                    
+                
+                
     # ordena la lista segun el algoritmo de ordenacion
     # bubble sort
     # cada vez que se intercambian dos elementos se dibuja la lista:
@@ -53,18 +79,19 @@ def bubbleSort(lista):
 
 if __name__ == "__main__":
 
-    plt.ion()
-    lista = createRandomList(15)
-    bubbleSort(lista)
-    isSorted(lista)
-    plt.show(block=True)
+    #plt.ion()
+    listatest = createRandomList(15)
+    print(listatest)
+    #print(bubbleSort(listatest))
+    print(isSorted(listatest))
+    #plt.show(block=True)
 
     # Listas de strings como casos test #
     # desactivar display() en bubbleSort()
-
+"""
     for test in open("stringTestCases.txt", 'r'):
         testList = list(test.replace(' ', ''))
         bubbleSort(testList)
         assert isSorted(testList), "Test %s " % (str(test))
 
-    print("string test cases passed")
+    print("string test cases passed")"""
